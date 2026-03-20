53a6844fa5783bde5e7ce7df7455350e9fea9e5b
Dagmawi Wube <dagmawi.wube+bveng@bazaarvoice.com>
2026-03-20 16:06:24 -0600

    firebase deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // This creates a static 'out' folder
+  images: { unoptimized: true }, // Required for static export
 };
 
 export default nextConfig;
